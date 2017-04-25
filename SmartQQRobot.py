1e09901eed8887f911f0178a807fd9c99a2ad981
pyshu <lius153@qq.com>
2017-04-25 23:08:40 +0800

    add login status

--- SmartQQRobot.py
+++ SmartQQRobot.py
@@ -16,14 +16,11 @@ class SmartQQRobot():
 
         self._session = requests.session()
         self._session.headers.update(self._headers)
-        self._cookies = {}
+        self._cookies_qrsig = ""
 
-        self._login_error = False
+        self._login_status = False
 
-    def _show_QRC(self):
-        url = "https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&amp;e=0&amp;l=M&amp;s=5&amp;d=72&amp;v=4&amp;t=0.6228904674882688"
-        content = self._session.get(url=url).content  #获取二维码数据（二进制）
-        self._cookies = requests.utils.dict_from_cookiejar(self._session.cookies)
+    def _show_QRC(self,content):
         if(content != None):
             root = tkinter.Tk()  #显示二维码
             root.title("扫描二维码")
@@ -34,15 +31,14 @@ class SmartQQRobot():
             label.pack()
             root.mainloop()
         else:
-            self._login_error = True
+            # self._login_error = True
             print("二维码获取失败.")
 
     def _get_ptqrtoken(self):
+        # ptqrtoken 计算
         token = 0
-        # cookies = self._cookies["qrsig"]
-        cookies = "R*Aa*UoGwYUesWfLuAiAJttOYU6sCJjmPjN8Tjr4wstWt*NtIa-TI-c-8RLcW*Bj"
-        for i in range(len(cookies)):
-            token += (token << 5) + ord(cookies[i])
+        for i in range(len(self._cookies_qrsig)):
+            token += (token << 5) + ord(self._cookies_qrsig[i])
         print(2147483647 & token)
         return 2147483647 & token
 
@@ -54,17 +50,30 @@ class SmartQQRobot():
               "&action=0-0-" + str(random.randint(1000, 30000)) +\
               "&mibao_css=m_webqq&t=1&g=1&js_type=0&js_ver=10216&login_sig=&pt_randsalt=2"
         print(url)
+        # 死循环检测扫码登陆状态
+        while 1:
+            content = self._session.get(url=url).content.decode("utf-8")
+            print(content)
+            # 登陆成功跳出循环
+            if "ptuiCB" not in content:
+                self._login_status = True
+                break
+            time.sleep(3)
 
     def _login(self):
+        url = "https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&amp;e=0&amp;l=M&amp;s=5&amp;d=72&amp;v=4&amp;t=0.6228904674882688"
+        content = self._session.get(url=url).content  #获取二维码数据（二进制）
+        self._cookies_qrsig = (requests.utils.dict_from_cookiejar(self._session.cookies))["qrsig"]
         #使用进程显示二维码，等待扫描。
-        p = Process(target=self._show_QRC)
+        p = Process(target=self._show_QRC,args=(content,))
         #启动进程 p
         p.start()
+        time.sleep(1)
         self._check_login_status()
-        time.sleep(10)
-        #结束进程 p
+        # 结束进程 p
         if p.is_alive():
             p.terminate()
+
 
     def msg_robot(self):
         pass
